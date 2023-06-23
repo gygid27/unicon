41ec2114100443c40dd72cb1980a8fb7ba013082
gygid27 <gygid27@naver.com>
2023-06-23 17:21:41 +0900

    2023.06.23 로또

--- 2023.06.23실습/static/js/arrary7.js
+++ 2023.06.23실습/static/js/arrary7.js
@@ -70,17 +70,40 @@
     -  다른 속성이라 하면 width, height, style 등등
       <div width="200">  -> 이거는 width라는 속성에 의해
       div의 크기에 영향을 준다.  하지만 dataset은 영향을 주지않는다.
-        git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
+
+      window.innerwidth; 브라우저의 너비
+      window.innerHeight; 브라우저의 화면 높이
+      window.outerWidth; 브라우저의 전체 너비
+      window.outHeight;  브라우저의 전체 높이
+    브라우저의 크기가 변경되면 동작하는 함수 - resize();
+
 */
+    window.onresize=function(){
+        var wd = window.innerWidth;
+        if( wd > 786){
+            var list = document.getElementsByClassName("menu_list")[0];
+            list.style.display="none";
+            list.dataset.show='1';
+           
+            // if()
+            // open_close();
+            // //열려있을 때만 동작하는 것이다.
+        }
+    }
+
 window.onload=function(){
+    var wd= window.innerWidth;
+    
     // 화면이 전부 로딩 되면 시작하는 함수
         var icon = document.getElementsByClassName("strapIcon");    
         icon[0].addEventListener("click", open_close);
+
+        content =document.querySelector("#content");
     }
     
     function open_close(){
-        var list = this.nextSibling;
+        var list = document.getElementsByClassName("strapIcon")[0].nextSibling;
+        //this = strapIcon 를 의미한다.
         var show = list.dataset.show;
         if(show==1){
             list.style.display="block";
@@ -89,6 +112,8 @@ window.onload=function(){
             list.style.display="none";
             list.dataset.show='1';
         }
+
+        
     
         // var isActive = list.classList.contains("list_active");
         // if(isActive)
@@ -96,4 +121,72 @@ window.onload=function(){
         // else
         //     list.classList.add("list_active");
     }
+   
+    function win_confirm(){
+        alert("당첨확인 클릭");
+    }
+
+    let content=null;
+
+    function make_num(){
+        var out="<table class='makeTable'>";
+
+        for(var n=1; n<=5; n++){
+        
+       
+            let lucky_num = new Array();
+
+            lucky_num.push(Math.floor(Math.random()*45)+1);
+            //첫번째 값을 미리 하나 줬기 때문에 var i=1로부터 시작하는 것;
+            for( var i=1; i<6; i++){
+                var num = Math.floor(Math.random()*45)+1;
+                if(lucky_num.indexOf(num) == -1){
+                    lucky_num.push(num);
+                }else{
+                    i--;
+                }// 중복안하게 하기 위해 --를 넣는것이다. 
+            }
+
+            lucky_num.sort(function(a,b){return a-b;})
+            //로또 숫자를 태그에 담아주기
+
+        
+            out += "<tr>";
+
+            out+="<td class='numTd'>"+n+".</td>"
+            for(var i=0; i<lucky_num.length; i++){
+                out += "<td class='numTd'>"+lucky_num[i]+"</td>";
+            }
+
+            out += "</tr>";
+
+            //짝,홀 갯수, 총합도 구해봅시다.
+            var even=0, odd=0;  //even 짝수, odd 홀수
+            //반복문을 돌린다.
+            var total=0;
+            for(var i=0; i<lucky_num.length; i++){
+                total += lucky_num[i];
+                if( lucky_num[i]%2 === 0){
+                    even++;
+                }else
+                    odd++;
+            }
+              
+            out += "<td colspan='7'>"+
+            "총합 : "+total+" "+
+            "홀/짝 : "+odd+"/"+even+"</td>";
+        }    
+        // 5번 반복하는 for문 끝
+        out += "</table>";
     
+       //출력
+        content.innerHTML= out;
+    }
+    
+    function num_count(){
+        alert("출현횟수");
+
+    }
+
+    //두개의 값을 계산하여 "+" 라면 자리를 바꿔주기(양수일때만 자리바뀜)
+    //"-" 라면 그대로 두기
