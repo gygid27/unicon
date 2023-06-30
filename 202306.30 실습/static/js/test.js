3baaf460d6060ad2963c48521c7b63bd03a76c67
gygid27 <gygid27@naver.com>
2023-06-30 15:41:09 +0900

    2023.06.30-1

---
+++ 202306.30 실습/static/js/test.js
@@ -0,0 +1,11 @@
+// var X = 5;
+// var Y = 10;
+
+// document.write( "x" + x + "y:"+ "<br>" + X+ parseInt(X+Y))
+
+
+// function 오빠야(){
+//    var res = document.getElementById("res")
+//    res.innerHTML="알려줘서 고마워";
+    
+// }
