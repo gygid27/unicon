dbf07c943bb1b52392bf63e2bcd4c113ed8d72ae
gygid27 <gygid27@naver.com>
2023-07-14 18:38:25 +0900

    2023.07.14 실습

--- 2023.07.12/static/js/zoo.js
+++ 2023.07.12/static/js/zoo.js
@@ -6,6 +6,37 @@
 // }
 
 window.onload=function(){
+
+    var pre = document.getElementsByClassName("prev")[0];
+    var next = document.getElementsByClassName("next")[0];
+    pre.addEventListener("click",function(){
+        var slideview = document.getElementsByClassName("eventslide")[0];
+        slideview.style.transform="translate3d(0px, 0px, 0px)";  //ul태그
+        
+
+
+        // var now_left = slideview.style.left.split("px")[0];
+        // if( now_left == 0) return;
+        // var left = Number(now_left) +312;  
+        // slideview.style.left=left+"px";
+
+
+        
+    });
+
+    next.addEventListener("click", function(){
+        var slideview = document.getElementsByClassName("eventslide")[0];
+        slideview.style.transform="translate3d(-1248px, 0px, 0px)";
+
+
+
+        // var now_left = slideview.style.left.split("px")[0];
+        // if(now_left == -936) return;
+        // var left = now_left -312; //  li 하나당 312px이기 때문에 
+        // slideview.style.left=left+"px";
+    });
+
+
    var search_bt = document.getElementsByClassName("search_button");
    search_bt[0].addEventListener("click", function(){
     var bar = document.querySelectorAll(".search_bar")[0];
