9e47c17fc9e2fbf4859c086d5e0c4760a33b8d86
gygid27 <gygid27@naver.com>
2023-07-31 15:58:36 +0900

    2023.07.31실습

--- 2023.07.31실습/static/js/input_get.js
+++ 2023.07.31실습/static/js/input_get.js
@@ -4,36 +4,54 @@
 //before - 선택한 태그의 앞쪽에 추가
 //after - 태그의 뒤쪽에 추가
 
+//remove - 선택한 태그를 삭제
+//empty - 선택한 태그안에 삭제
+
+//removeClass - 선택한 태그에 지정한 클래스 삭제
+//addClass - 선택한 태그에 지정한 클래스 추가
 
 
 
 
+let ri=1;
 
 $(function(){
-  $("#setImage").click(function(){
-    var data = $("#imageName").val();
-    // alert(data);
+    $("#setImage").click(function(){
+        var data = $("#imageName").val()+(ri++);
+        
+        $("#gallery").append("<div class='img_box'><img src='"+data+"'></div>");
+    });
 
-    $("#gallery").append("<div class = 'img_box'><img src= '"+data+"'></div>");
-  });
+    $("#3n").click(function(){
+        $("#gallery").removeClass("grid-4n");
+        $("#gallery").addClass("grid-3n");
+        $("#gallery").removeClass("slide-active");
+        $("#gallery").addClass("slide-off");
+        $("#gallery").append($(".img_box"))
+        //$("#gallery").css("grid-template-columns","repeat(3,1fr)");
+    });
+   
+    $("#4n").click(function(){
+        $("#gallery").removeClass("grid-3n");
+        $("#gallery").addClass("grid-4n");
+        $("#gallery").removeClass("slide-active");
+        $("#gallery").addClass("slide-off");
+        $("#gallery").append($(".img_box"));
+    });
 
-  $("#3n").click(function(){
+// https://loremflickr.com/g/1200/400/paris
+// 슬라이드
+
+   $("#slide").click(function(){
     $("#gallery").removeClass("grid-4n");
-    $("#gallery").addClass("grid-3n");
-    // $("#gallery").css("grid-template")
-  });
+    $("#gallery").removeClass("grid-3n");
+    $("#gallery").removeClass("slide-off");
+    $("#gallery").addClass("slide-active");
+    $("#slide_box").append($(".img_box"));
+    var len = $(".img_box").length;
+    $("#slide_box").css("width", len*1200+"px");
 
-  $("#4n").click(function(){
-    $("gallery").removeClass("grid-3n");
-    $("gallery").addClass("grid-4n");
-    
-  });
-
-  $("#slide").click(function(){
-    $("gallery").removeClass("grid-3n");
-    $("gallery").addClass("grid-4n");
-    
-  });
+   });
 
 });
 
