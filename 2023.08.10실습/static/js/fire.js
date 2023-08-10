2c5a731c70afc2a5b4e7b9b21b3416bf450adfc5
gygid27 <gygid27@naver.com>
2023-08-10 18:56:33 +0900

    2023.08.10

--- 2023.08.10실습/static/js/fire.js
+++ 2023.08.10실습/static/js/fire.js
@@ -1,4 +1,4 @@
-let data = []; // json 데이터 저장할 변수
+let data = []; //json저장할 변수
 let fire_stat = new Object();
 let color = [
     '#FF6347',
@@ -15,44 +15,25 @@ let color = [
     '#7CFC00',
     '#008080',
 ];
+
 async function getData() {
     var temp = await fetch('./traffic.json').then((r) => r.json());
-    //console.log(temp);
-    return temp.body.items;
+    console.log();
+    return temp.body.item;
 }
 
 $(async function () {
     data = await getData();
+    var total = (bike = 0);
     $.each(data, function (i, item) {
         total += Number(item.gutCo); //전체 사고건수
         bike += item.rlifAcdAsmCdNm === '오토바이사고' ? Number(item.gutCo) : 0;
-        if (item.rsacGutFsttOgidNm in fire_stat) {
+        if (item.rlifAcdAsmCdNm in fire_stat) {
             fire_stat[item.rsacGutFsttOgidNm].출동건수 += item.gutCo;
-            fire_stat[item.rsacGutFsttOgidNm].환자수 += item.trnfPcnt;
-        } else {
-            fire_stat[item.rsacGutFsttOgidNm] = { 출동건수: Number(item.gutCo), 환자수: Number(item.trnfPcnt) };
+            fire_stat[item.rsacGutFsttOgidNm].환자수 += item.trnfpcnt;
+        }else{
+            fire_stat[item.rsacGutFsttOgidNm] = {출동건수:Number(item.gutCo)
+            환자수:Number(item.trnfPcnt)};
         }
     });
 });
-
-var ctx2 = $('#pi')[0].getContext('2d');
-
-var pc = 1 / (total / bike);
-var bike_deg = 270 + 360 * pc;
-bike_deg = Math.round((bike_deg > 360 ? bike_deg - 360 : bike_deg) * 100) / 100;
-
-ctx2beginpath();
-ctx2.moveTo(350, 350);
-ctx2.arc(350, 350, 300, (270 * Math.PI) / 180, (bike_deg * Math.PI) / 180, false);
-ctx2.fillStyle = 'yellow';
-ctx2.fillStyle();
-
-ctx2beginpath();
-ctx2.moveTo(350, 350);
-ctx2.arc(350, 350, 300, (270 * Math.PI) / 180, (bike_deg * Math.PI) / 180, false);
-ctx2.fillStyle = 'orange';
-ctx2.fillStyle();
-
-ctx2.fillStyle = '#000';
-ctx.font = '30px Arial';
-ctx.fillText(Math.round(pc * 1000) / 10 + '%', 450, 200);
