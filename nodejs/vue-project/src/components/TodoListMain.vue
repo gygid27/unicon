74b7d7d23b56804aa64f6cdfe21b28bd9c4bf610
gygid27 <gygid27@naver.com>
2023-09-07 09:14:31 +0900

    0907

---
+++ nodejs/vue-project/src/components/TodoListMain.vue
@@ -0,0 +1,85 @@
+<template>
+  <TodoListMenu v-on:change-filter="onChangeFilter" class="p-0" />
+  <div v-for="key in Object.keys(filter_todos)" :key="key" class="mb-3">
+    <div v-if="use_category">
+      <em>{{ key }}</em>
+    </div>
+    <TodoList :data="filter_todos[key]" />
+  </div>
+  <div class="my-2 mt-5">
+    <span style="background-color: pink">&nbsp;</span>&nbsp;
+    <strong>처리하지 못한 할일</strong>
+  </div>
+  <TodoList :data="pending_todos" />
+</template>
+
+<script>
+import TodoListMenu from '../components/TodoListMenu.vue'
+import TodoList from '../components/TodoList.vue'
+
+import { ref, provide, inject, watch } from 'vue'
+import { useFilter } from '../stores/filter'
+
+export default {
+  name: 'TodoListMain',
+  components: { TodoListMenu, TodoList },
+
+  setup(props) {
+    const { getPendingTodos, getActiveToday, getCompletedToday, getAllTodayTodo, getAllTodo } =
+      useFilter()
+    const filter = ref(0)
+    const filter_todos = ref(0)
+    const pending_todos = ref([])
+    const use_category = ref(false)
+    const todos = inject('todos')
+
+    const filters = {
+      0: {
+        str: '할일들',
+        func: getActiveToday,
+        category: false
+      },
+      1: {
+        str: '미션완료',
+        func: getCompletedToday,
+        category: false
+      },
+      2: {
+        str: '오늘의 기록',
+        func: getAllTodayTodo,
+        category: false
+      },
+      3: {
+        str: '오늘 할 일들',
+        func: getAllTodo,
+        category: false
+      }
+    }
+    provide('filters', filters)
+    const groupBy = (todos) => {
+      return todos.reduce((acc, cur) => {
+        acc[cur['date']] = acc[cur['date']] || []
+        acc[cur['date']].push(cur)
+      }, {})
+    }
+    const onChangeFilter = (filter_idx) => {
+      filter.value = Number(filter_idx)
+    }
+    watch(
+      [() => filter.value, todos.value],
+      ([new_filter, new_todos], [old_filter, old_todos]) => {
+        pending_todos.value = getPendingTodos(todos)
+        if (typeof new_filter != 'undefined') {
+          let temp_todos = filters[new_filter].func(todos)
+          filter_todos.value = groupBy(temp_todos)
+          use_category.value = filters[new_filter].category
+        }
+      },
+      { immediate: true }
+    )
+    return { filter, pending_todos, filter_todos, use_category, onChangeFilter }
+  }
+}
+</script>
+
+<style></style>
---
+++ nodejs/vue-project/src/views/TodoListContainer.vue
@@ -0,0 +1,56 @@
+<template>
+  <TodoListNew />
+  <section class="container">
+    <div class="row justify-content-center m-2">
+      <TodoListMain />
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref, readonly, provide } from 'vue'
+import { useStorage } from '../stores/storage'
+import TodoListMain from '../components/TodoListMain.vue'
+import TodoListNew from '../components/TodoListNew.vue'
+export default {
+  name: 'TodoListContainer',
+  components: {
+    TodoListNew,
+    TodoListMain
+  },
+  setup() {
+    const todos = ref([])
+    const { loadTodos, saveTodos, storage_id } = useStorage()
+    provide('todos', readonly(todos))
+
+    const initTodos = (init_todos) => {
+      todos.value = init_todos
+    }
+    const addTodo = (job, date) => {
+      //새로운 할일 등록
+      todos.value.push({
+        id: storage_id.value++,
+        job: job,
+        date: date,
+        completed: false
+      })
+      saveTodos(todos)
+    }
+    const removeTodo = (id) => {
+      todos.value.splice(id, 1)
+      todos.value.forEach((todo, idx) => {
+        todo.id = idx
+      })
+      saveTodos(todos)
+    }
+    const completeTodo = (id) => {
+      todos.value.filter((todo) => todo.id == id).completed = true
+      saveTodos(todos)
+    }
+    provide('addTodo', addTodo)
+    provide('removeTodo', removeTodo)
+    provide('completeTodo', completeTodo)
+  }
+}
+</script>
+<style></style>
